018043269b45cf2932c1954148ff6364ff13a789
PashaKopka <pashakopka216@gmail.com>
2020-10-29 16:25:14 +0200

    some refactor of existing code

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from __future__ import unicode_literals
 
-from xxlimited import error
-
 from aiogram import Bot, Dispatcher, executor, types
 
 import os
 import youtube_dl
 import logging
 
-API_TOKEN = 'set your api'
+API_TOKEN = '1329098332:AAF1yP2menisi0uE3P5CSP5MnvcuNoiW-iY'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,32 +19,40 @@ async def send_welcome(message: types.Message):
     await message.reply('Hi!\nI\'m EchoBot!\nPowered by aiogram.')
 
 
+def set_options():
+    return {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+
+def download_music(url):
+    options = set_options()
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        filename = f'{info_dict.get("title", None)}-{info_dict.get("id", None)}.mp3'
+        ydl.download([url])
+
+    with open(filename, 'rb') as f:
+        data = f.read()
+
+    return data, filename
+
+
 @dp.message_handler(commands=['music'])
 async def echo(message: types.Message):
-    print(message.text[7:30])
     if message.text == '/music':
         await message.answer('You must send url:\n/music https://www.youtube.com/example')
     if message.text[7:31] == 'https://www.youtube.com/':
-        ydl_opts = {
-            # 'outtmpl': 'test',
-            'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(message.text[7:], download=False)
-            video_title = info_dict.get('title', None)
-            video_id = info_dict.get("id", None)
-            ydl.download([message.text[7:]])
-
-        with open(f'{video_title}-{video_id}.mp3', 'rb') as f:
-            data = f.read()
+        data, filename = download_music(message.text[7:])
 
         await message.answer_audio(data)
-        os.remove(f'{video_title}-{video_id}.mp3')
+        os.remove(filename)
 
 
 if __name__ == '__main__':
